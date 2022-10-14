a66ef6ecf9e7a280c50e688c3f5519f72eaab6c4
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-10-14 16:10:54 -0500

    Add linter Styles

--- src/Modules/createGame.js
+++ src/Modules/createGame.js
@@ -8,7 +8,7 @@ fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
-});
+})
 
 .then((response) => response.json());
 
